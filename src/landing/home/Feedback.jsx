9510ea8e89dd42f1dd9878ec70973a40af0864d5
Alfredo Castellano <130506421+ALCAMOjr@users.noreply.github.com>
2025-04-09 09:02:26 -0600

    Update Feedback.jsx

--- src/landing/home/Feedback.jsx
+++ src/landing/home/Feedback.jsx
@@ -136,14 +136,14 @@ export default function CustomerFeedback() {
             <button
               onClick={prev}
               disabled={currentIndex === 0}
-              className="w-10 h-10 flex items-center justify-center border border-blue rounded-full hover:bg-gray-100 disabled:opacity-30"
+              className="w-10 h-10 flex items-center justify-center border border-blue rounded-full hover:bg-white-100 disabled:opacity-30"
             >
               <ArrowLeftIcon className="w-5 h-5 text-gray-700" />
             </button>
             <button
               onClick={next}
               disabled={currentIndex >= maxIndex}
-              className="w-10 h-10 flex items-center justify-center bg-blue text-white rounded-full hover:bg-gray-100 disabled:opacity-30"
+              className="w-10 h-10 flex items-center justify-center bg-blue text-white rounded-full hover:bg-white-100 disabled:opacity-30"
             >
               <ArrowRightIcon className="w-5 h-5" />
             </button>
